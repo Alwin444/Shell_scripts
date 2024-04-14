c684880ac381d613db58d7aa7528d9902318c902
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-04-14 13:42:16 +0530

    Update DNS_Exploder.sh

--- DNS_Exploder.sh
+++ DNS_Exploder.sh
@@ -43,52 +43,28 @@ selection()
 {
  case $option in
   1 )
-    rec1=$(dig $domain A +short)   
-
-    echo -e "-------------------------------\n${red}A record of $domain:${reset}${bold}$rec1${reset}\n-------------------------------";;
-
+     rec1=$(dig $domain A +short)
+     echo -e "-------------------------------\n${red}A record of $domain:${reset}${bold}$rec1${reset}\n-------------------------------";;
   2 )
-
-    rec2=$(dig $domain AAAA +short)   
-
-    echo -e "-------------------------------\n${red}AAAA record of $domain:${reset}${bold}$rec2${reset}\n-------------------------------";;
-
+     rec2=$(dig $domain AAAA +short)   
+     echo -e "-------------------------------\n${red}AAAA record of $domain:${reset}${bold}$rec2${reset}\n-------------------------------";;
   3 )
-
-    rec3=$(dig $domain MX +short)   
-
-    echo -e "-------------------------------\n${red}MX record of $domain:${reset}${bold}$rec3${reset}\n-------------------------------";;
-
+     rec3=$(dig $domain MX +short)   
+     echo -e "-------------------------------\n${red}MX record of $domain:${reset}${bold}$rec3${reset}\n-------------------------------";;
   4 )
-
-    rec4=$(dig $domain CNAME +short)   
-
-    echo -e "-------------------------------\n${red}CNAME record of $domain:${reset}${bold}$rec4${reset}"
-
-    rec4_1=$(dig $domain TXT +short)
-
-    echo -e "\n${red}TXT record of $domain:${reset}${bold}$rec4_1${reset}\n-------------------------------";;
-
+     rec4=$(dig $domain CNAME +short)
+     echo -e "-------------------------------\n${red}CNAME record of $domain:${reset}${bold}$rec4${reset}"
+     rec4_1=$(dig $domain TXT +short)
+     echo -e "\n${red}TXT record of $domain:${reset}${bold}$rec4_1${reset}\n-------------------------------";;
   5 )
-
-    rec5=$(dig $domain NS +short)   
-
-    echo -e "-------------------------------\n${red}NS(subdomains) record of $domain:\n${reset}${bold}$rec5${reset}\n-------------------------------";;
-
+     rec5=$(dig $domain NS +short)   
+     echo -e "-------------------------------\n${red}NS(subdomains) record of $domain:\n${reset}${bold}$rec5${reset}\n-------------------------------";;
   6 )
-
-    dns;;
-
+     dns;;
   * )
-
-    echo "${purple}Invalid option${reset}"
-
-    opt
-
-    selection;;
-
+     echo "${purple}Invalid option${reset}"
+     opt
+     selection;;
  esac
-
 }
-
 selection
