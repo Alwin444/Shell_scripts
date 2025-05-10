cc597be12ccea71d56c1e8109a36ac13c7198e1e
Alwin George <150264978+Alwin444@users.noreply.github.com>
2025-05-10 22:28:48 +0530

    Delete Single-Directorypather.sh

--- Single-Directorypather.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo "Enter the directory name: "
-read directory_path
-cd /$directory_path
-echo "The directory is been found, present working directory is $(pwd)"
-echo "Files: "
-ls
